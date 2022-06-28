029ad040f45926b9c7dbb387f83e49c6fe5febb6
12roshan12 <roshansharma7502@gmail.com>
2022-06-28 16:28:00 +0545

    ch

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  screenWidth: any;
+  screenHeight: any;
+
+  
+  ngOnInit(): void {
+    this.screenWidth = window.innerWidth;  
+    this.screenHeight = window.innerHeight; 
+
+    console.log(this.screenHeight)
+    console.log(this.screenWidth)
+  }
   title = 'frontend';
 }
