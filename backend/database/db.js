d7c1617e79754da0eb41aa6324b7246ee8422096
12roshan12 <roshansharma7502@gmail.com>
2022-06-29 13:44:49 +0545

    backend

---
+++ backend/database/db.js
@@ -0,0 +1,17 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password:"",
+    port: 3306,
+    database:"roomfinder"
+});
+
+con.connect(function(err){
+    if(err) throw err;
+    console.log("Connected")
+})
+
+
+module.exports = con;
